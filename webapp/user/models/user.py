b95d1ee82444227a7be8bc9c819123148875b9dc
KimChanJin97 <102044895+KimChanJin97@users.noreply.github.com>
2023-06-22 00:19:51 +0900

    test: limit push into master branch & pr template

--- webapp/user/models/user.py
+++ webapp/user/models/user.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class User(models.Model):
-    pass
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
