37bfc9c9b945b7cd79e0c790611b53b58bf1ece8
김유림 <belebt01@kookmin.ac.kr>
2023-06-25 20:48:33 +0900

    feat:outsourcingModels Basic

--- webapp/outsourcing/models/field.py
+++ webapp/outsourcing/models/field.py
@@ -2,4 +2,21 @@ from django.db import models
 
 
 class Field(models.Model):
-    pass
+    class Meta:
+        db_table = 'Field'
+        verbose_name = 'fieldElement'
+        verbose_name_plural = 'fieldElements'
+        ordering = ['id']
+
+
+    field = models.CharField(
+        verbose_name='작업분야',
+        max_length=20,
+        null=False,
+        blank=False
+    )
+
+
+    def __str__(self):
+        return self.field
+    
--- webapp/outsourcing/models/tag.py
+++ webapp/outsourcing/models/tag.py
@@ -2,4 +2,29 @@ from django.db import models
 
 
 class Tag(models.Model):
-    pass
+    class Meta:
+        db_table = 'OutsourcingTags'
+        verbose_name = 'outsourcingTag'
+        verbose_name_plural = 'outsourcingTags'
+        ordering = ['id']
+
+
+    outsourcing = models.ForeignKey(
+        'outsourcing.Outsourcing',
+        verbose_name='외주',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+
+    tag = models.CharField(
+        verbose_name='외주태그',
+        max_length=20,
+        null=False,
+        blank=False
+    )
+
+
+    def __str__(self):
+        return self.portfolio
+    
